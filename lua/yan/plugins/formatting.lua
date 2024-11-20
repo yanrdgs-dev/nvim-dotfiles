de589ee3605282ab592320c9b0b219d805014e25
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2024-11-19 23:42:03 -0300

    Added Rust support

--- lua/yan/plugins/formatting.lua
+++ lua/yan/plugins/formatting.lua
@@ -20,6 +20,7 @@ return {
 				liquid = { "prettier" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
+				rust = { "rustfmt" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
